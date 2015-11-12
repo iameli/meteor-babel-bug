55aa86036b7d0d1851caf17414c5ea945687e685
Eli Mallon <eli.mallon@csats.com>
2015-11-11 22:09:46 -0800

    Basic demo of this bug

---
+++ test.jsx
@@ -0,0 +1,11 @@
+
+// Just a quick test of both of my presets here.
+
+// This should become a "require" statement
+import fs from 'fs';
+
+const uselessReactFunction = () => {
+  return <div />
+}
+
+console.error('Both presets worked without error.');
